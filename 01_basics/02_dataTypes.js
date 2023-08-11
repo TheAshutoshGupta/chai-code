745c576e9e1b358a8507d4a2ae53f0d6fa20fd84
Ashutosh <Ashutoshg479@gmail.com>
2023-08-11 15:34:57 +0530

    first commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";   //treal all Js code as newer version
+//alert(3+3) //wont work cz we are using nodejs, not browser
+
+console.log(3+3)
+console.log('hitesh')
+
+
+//dynamic nature
+let name='hitesh' //string
+let age=21 //number
+let isLoggedIn=false //boolean
+
+//PIRMITIVE DATA TYPE
+
+//number=>2 to power 53
+//bigint
+//string=> ""
+//boolean=>true/false
+//  null  =>  standalone value
+//  undefined  => n value
+//symbol => unique
+
+
+//OBJECT
+
+console.log(typeof age)
+
+
+console.log(typeof null) 
+//output :- object  ,why??
---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId=14453
+let accountEmail="ashutooshgupta@gmail.com"
+let accountPass="12345"
+accountName='ashutosh'
+let check
+
+console.table([accountEmail,accountId,accountName,accountPass,check])
+/*
+we dont use var now because of function scope and block scope
+if we dont use value in variable, it will store "undefined"
+*/
