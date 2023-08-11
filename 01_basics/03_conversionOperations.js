5df7c7a397611ec657b933454030124708b71454
Ashutosh <Ashutoshg479@gmail.com>
2023-08-11 15:58:59 +0530

    config

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let score = '33abs'
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber=Number(score) //type conversion to number
+console.log(typeof valueInNumber) //again check
+console.log(valueInNumber);  
+
+//"33" =>33
+//"33abs"=>NaN
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 =>true; 0 => false
+// " "=> false
+//"hitesh" => true
+
+let someNumber = 33
+let stringNUm=String(someNumber)
+console.log(typeof stringNUm)
