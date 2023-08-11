3737cd742b4516f6ab7fe4ee97b72a9840e071b7
Ashutosh <Ashutoshg479@gmail.com>
2023-08-11 17:00:25 +0530

    config

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,24 +1,55 @@
-let score = '33abs'
+// let score = '33abs'
 
-console.log(typeof score)
-console.log(typeof(score))
+// // console.log(typeof score)
+// // console.log(typeof(score))
 
-let valueInNumber=Number(score) //type conversion to number
-console.log(typeof valueInNumber) //again check
-console.log(valueInNumber);  
+// let valueInNumber=Number(score) //type conversion to number
+// // console.log(typeof valueInNumber) //again check
+// // console.log(valueInNumber);  
 
-//"33" =>33
-//"33abs"=>NaN
+// //"33" =>33
+// //"33abs"=>NaN
 
 
-let isLoggedIn = 1
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let isLoggedIn = 1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn)
 
-// 1 =>true; 0 => false
-// " "=> false
-//"hitesh" => true
+// // 1 =>true; 0 => false
+// // " "=> false
+// //"hitesh" => true
 
-let someNumber = 33
-let stringNUm=String(someNumber)
-console.log(typeof stringNUm)
+// let someNumber = 33
+// let stringNUm=String(someNumber)
+// // console.log(typeof stringNUm)
+
+// // *********************operations**********************
+
+// let value =3 
+// let negValue = -value
+// // console.log(negValue)
+
+// // console.log(2+2)
+// // console.log(2-2)
+// // console.log(2**2)
+// // console.log(2%2)
+// // console.log(2+2)
+// // console.log(2*2)
+
+// let str1 = "hello"
+// let str2="world"
+// let str3=str1+str2
+// console.log(str3)
+
+// console.log(2+"2")
+// console.log(2+2 +"2")
+// console.log(+ true);
+// console.log(+"")
+
+let num1,num2,num3
+
+num1=num2=num3=2+2 
+
+let gameCounter = 100
+let k=++gameCounter
+console.log(k)
