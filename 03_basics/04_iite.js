fbab23c83cc7d9776aa60f84b8726d659cf9cf31
Ashutosh <Ashutoshg479@gmail.com>
2023-08-17 04:00:34 +0530

    config

---
+++ 03_basics/04_iite.js
@@ -0,0 +1,13 @@
+(function(){ //names iife
+    console.log("ashutosh")
+})(); //using semi colon is important here because it needs to get stopped at some point of time
+
+/* 
+syntax=(define the function)(execution call)
+*/
+
+
+//unnamed iife
+/* some more ways for iife */
+(()=>{console.log("ashutosh")})();
+((name)=>{console.log(name)})("ashutosh");
